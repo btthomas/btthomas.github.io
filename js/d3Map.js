caea8e295dd6b2cf4cfffa60ff64b00c41311e09
Blake <bthomas@onlocationinc.com>
2015-09-19 19:22:15 -0400

    data files

--- js/d3Map.js
+++ js/d3Map.js
@@ -1,8 +1,9 @@
 var svgHeight,
     svgWidth;
 
-var hMargin = 20,
-    vMargin = 20;
+var hMargin = 30,
+    vMargin = 15;
+    scroll = 15;
 
 $(window).load(initialize)
 
@@ -10,13 +11,55 @@ function initialize() {
     
     console.log($(window).width())
     
-    svgHeight = $(window).height() - 2 * vMargin;
+    svgHeight = $(window).height() - $('#pre').position().top - 3 *vMargin;
     svgWidth = $(window).width() - 2 * hMargin;
     
-    d3.select('#mapDiv')
-        .append('SVG')
+    d3.select('#mapSvg')
         .attr({height: svgHeight,
                width: svgWidth,
-               class: 'svg'
+               class: 'svg',
         });
+    
+    //load the data
+    
+    //do the svg
+    makeTheMap();
+}
+
+function resizeSVG() {
+    svgHeight = $(window).height() - $('#pre').position().top - vMargin;
+    svgWidth = $(window).width() - 2 * hMargin - scroll;
+    
+    //then resize it
+    d3.select('#mapSvg')
+        .attr({height: svgHeight,
+               width: svgWidth
+        })
+}
+
+function makeTheMap() {
+    var projection = d3.geo.albersUsa()
+        .scale(1000)
+        .translate([svgWidth / 2, svgHeight / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+        
+    var svg = d3.select('#mapSvg');
+    
+    svg.append('svg:rect')
+        .attr({x: 0,
+                y: 0,
+                width: svgWidth,
+                height: svgHeight,
+                id: 'svgBackground'
+        })
+        .style('fill', 'lightblue');
+
+    svg.append('text')
+        .attr({x: 100,
+               y: 100,
+               id: 'svgText1'
+        })
+        .text('Blake!');   
 }
