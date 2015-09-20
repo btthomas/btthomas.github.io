af3f1bab61c0a06162131842b280d6741ef46af2
Blake <bthomas@onlocationinc.com>
2015-09-19 20:10:03 -0400

    state map

--- js/d3Map.js
+++ js/d3Map.js
@@ -1,5 +1,6 @@
 var svgHeight,
-    svgWidth;
+    svgWidth,
+    stateData;
 
 var hMargin = 30,
     vMargin = 15;
@@ -14,24 +15,14 @@ function loadTheData() {
             console.log(err);
             return;
         }
-        console.log(data)
+        stateData = data;
+        makeTheMap();
     });
 }
 
 function initialize() {
-    loadTheData();
-    
-    svgHeight = $(window).height() - $('#pre').position().top - 3 *vMargin;
-    svgWidth = $(window).width() - 2 * hMargin;
-    
-    d3.select('#mapSvg')
-        .attr({height: svgHeight,
-               width: svgWidth,
-               class: 'svg',
-        });
-
-        //do the svg
-    makeTheMap();
+    loadTheData();    
+    resizeSVG();
 }
 
 function resizeSVG() {
@@ -55,19 +46,10 @@ function makeTheMap() {
         
     var svg = d3.select('#mapSvg');
     
-    svg.append('svg:rect')
-        .attr({x: 0,
-                y: 0,
-                width: svgWidth,
-                height: svgHeight,
-                id: 'svgBackground'
-        })
-        .style('fill', 'lightblue');
-
-    svg.append('text')
-        .attr({x: 100,
-               y: 100,
-               id: 'svgText1'
-        })
-        .text('Blake!');   
+    svg.selectAll('path')
+        .data(stateData.features)
+      .enter()
+        .append('path')
+          .attr('d', path)
+          .attr('class', 'state');    
 }
