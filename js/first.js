d89a7654f26b043e1f9af5d5ae1ca6896213f5c0
Blake <bthomas@onlocationinc.com>
2015-12-12 20:24:38 -0500

    style, organization

--- js/first.js
+++ js/first.js
@@ -1,51 +1,115 @@
 ;'use strict'
 
 $('.noJavascript').remove();
-
-var colors = [
-    'gray',
-    'CornflowerBlue',
-    'orange',
-    'red',
-    'yellow',
-    'MediumSpringGreen',
-    'HotPink'
-];
+var colorList = [];
 var counter = 0;
-
-$('#svg').attr('width', '100%');
-var widthPerBar = $('#svg').width() / colors.length;
 var barHeight = 300;
+var playing = true;
+var widthPerBar;
 
-function tick() {
-  if(!(counter++ % 60)) shuffle();
-  draw();
-  requestAnimationFrame(tick);
-};
+setUp();
 tick();
 
+function setUp() {
+    $('#svg').attr('width', '100%');
+    widthPerBar = $('#svg').width() / 7;
+
+    var paragraph = d3.select('#content').insert('p','#svgDiv')
+        .attr({
+            id: 'pControl',
+            'class': 'text-center'
+        });
+
+    var toAppend = ['Play', 'Pause', 'Color'];
+    paragraph.selectAll('span')
+        .data(toAppend)
+      .enter().append('span')
+        .text(function(d) {return d})
+        .classed('space', true)
+        .on('click', click);
+        
+    var initColors = [
+        'gray',
+        'CornflowerBlue',
+        'orange',
+        'red',
+        'yellow',
+        'MediumSpringGreen',
+        'HotPink'
+    ];
+
+    initColors.forEach( function(d) {
+        colorList.push(d3.rgb(d).toString());
+    });
+    
+    draw();
+}
+
+function click(d) {
+    
+    switch(d) {
+        case 'Play':
+            play();
+            break;
+        case 'Pause':
+            pause();
+            break;
+        case 'Color':
+            color();
+            break;
+        default:
+            console.error('click error', this);
+    }
+}
+
+function play() {
+    playing = true;
+    tick();
+}
+
+function pause() {
+    playing = false;
+    counter = -1;
+}
+
+function color() {
+    //generate 6 random colors plus a random gray
+    var r = Math.floor(256 * Math.random());
+    var g, b;
+    
+    colorList = [d3.rgb(r, r, r).toString()];
+    
+    for(var i = 0; i < 6; i++) {
+        r = Math.floor(256 * Math.random());
+        g = Math.floor(256 * Math.random());
+        b = Math.floor(256 * Math.random());
+        colorList.push(d3.rgb(r,g,b).toString());
+    }    
+    draw();
+}
+
+function tick() {
+  if(!(counter++ % 60)) { counter -= 60; shuffle(); }
+  draw();
+  if(playing) requestAnimationFrame(tick);
+};
+
 function shuffle() {
-  var copy = [], n = colors.length, i;
-
-  // While there remain elements to shuffle…
+  var copy = [], n = colorList.length, i;
   while (n) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * colors.length);
-
-    // If not already shuffled, move it to the new array.
-    if (i in colors) {
-      copy.push(colors[i]);
-      delete colors[i];
+    i = Math.floor(Math.random() * colorList.length);
+    if (i in colorList) {
+      copy.push(colorList[i]);
+      delete colorList[i];
       n--;
     }
   }
-  colors = copy;
+  colorList = copy;
 } 
 
 function draw() {
     var join = d3.select('#svg').selectAll('rect')
-        .data(colors, function(d) {return d;});
+        .data(colorList, function(d) {return d;});
     join.enter()
         .append('rect');
     join.attr('x', function(d,i) {return widthPerBar * i; })
