3e7667e58b0c5d6fb8cec4dd41478a305a9ca545
blake thomas <blaketaylorthomas@gmail.com>
2017-07-27 12:17:19 -0400

    update 1 to d3 v4

--- js/first.js
+++ js/first.js
@@ -1,10 +1,22 @@
 ;'use strict'
 
-var colorList = [];
-var counter = 0;
-var barHeight = 300;
-var playing = true;
-var widthPerBar;
+const interval = 2000;
+let lastShuffle = 0;
+let playing = true;
+
+const barHeight = 300;
+const bars = 7;
+let widthPerBar;
+
+let colorList = [
+  'gray',
+  'CornflowerBlue',
+  'orange',
+  'red',
+  'yellow',
+  'MediumSpringGreen',
+  'HotPink'
+];
 
 window.onload = function() {
   noJavascript();
@@ -13,101 +25,90 @@ window.onload = function() {
     setUp,
     0
   );
-  tick();
+  tick(0);
 }
 
 function resize() {
-    //set svg to fill div
-    d3.select('#svg').attr('width', '100%');  
+  d3.select('#svg').attr('width', '100%');  
 }
 
 function setUp() {
-  
-    //get 1/7th this width in pixels
-    widthPerBar = document.querySelector('#svg').width.baseVal.value / 7;
+  //get 1/barth this width in pixels
+  widthPerBar = document.querySelector('#svg').width.baseVal.value / bars;
 
-    var paragraph = d3.select('#content').insert('p','#svgDiv')
-        .attr({
-            id: 'pControl',
-            'class': 'text-center'
-        });
+  const paragraph = d3.select('#content')
+    .insert('p','#svgDiv')
+    .attr('id', 'pControl')
+    .attr('class', 'text-center');
 
-    var toAppend = ['Play', 'Pause', 'Color'];
-    paragraph.selectAll('span')
-        .data(toAppend)
-      .enter().append('span')
-        .text(function(d) {return d})
-        .classed('space', true)
-        .on('click', click);
-        
-    var initColors = [
-        'gray',
-        'CornflowerBlue',
-        'orange',
-        'red',
-        'yellow',
-        'MediumSpringGreen',
-        'HotPink'
-    ];
-
-    initColors.forEach( function(d) {
-        colorList.push(d3.rgb(d).toString());
-    });
-    
-    draw();
+  const toAppend = ['Play', 'Pause', 'Color'];
+  paragraph.selectAll('span')
+    .data(toAppend)
+    .enter()
+    .append('span')
+      .text(d => d)
+      .classed('space', true)
+      .on('click', click);
+            
+  draw();
 }
 
 function click(d) {
     
-    switch(d) {
-        case 'Play':
-            play();
-            break;
-        case 'Pause':
-            pause();
-            break;
-        case 'Color':
-            color();
-            break;
-        default:
-            console.error('click error', this);
-    }
+  switch(d) {
+    case 'Play':
+      play();
+      break;
+    case 'Pause':
+      pause();
+      break;
+    case 'Color':
+      color();
+      break;
+    default:
+      console.error('click error', this);
+  }
 }
 
 function play() {
-    playing = true;
-    tick();
+  playing = true;
+  lastShuffle = performance.now()
+  tick(lastShuffle);
 }
 
 function pause() {
-    playing = false;
-    counter = -1;
+  playing = false;
+  counter = -1;
 }
 
 function color() {
-    //generate 6 random colors plus a random gray
-    var r = Math.floor(256 * Math.random());
-    var g, b;
-    
-    colorList = [d3.rgb(r, r, r).toString()];
-    
-    for(var i = 0; i < 6; i++) {
-        r = Math.floor(256 * Math.random());
-        g = Math.floor(256 * Math.random());
-        b = Math.floor(256 * Math.random());
-        colorList.push(d3.rgb(r,g,b).toString());
-    }    
-    draw();
+  let r, g, b
+
+  for(let i = 0; i < bars; i++) {
+    r = Math.floor(256 * Math.random());
+    g = Math.floor(256 * Math.random());
+    b = Math.floor(256 * Math.random());
+    colorList[i] = d3.rgb(r,g,b).toString();
+  }    
+  draw();
 }
 
-function tick() {
-  if(!(counter++ % 60)) { counter -= 60; shuffle(); }
-  draw();
-  if(playing) requestAnimationFrame(tick);
+function tick(timestep) {
+
+  if(timestep > interval + lastShuffle) {
+    lastShuffle = timestep;
+    shuffle();
+    draw();
+  }
+  if(playing) {
+    requestAnimationFrame(tick)
+  }
 };
 
 function shuffle() {
-  var copy = [], n = colorList.length, i;
+  let copy = []
+  let n = colorList.length
+  let i;
   while (n) {
     i = Math.floor(Math.random() * colorList.length);
     if (i in colorList) {
@@ -120,13 +121,14 @@ function shuffle() {
 } 
 
 function draw() {
-    var join = d3.select('#svg').selectAll('rect')
-        .data(colorList, function(d) {return d;});
-    join.enter()
-        .append('rect');
-    join.attr('x', function(d,i) {return widthPerBar * i; })
-        .attr('y', 0)
-        .attr('width',widthPerBar)
-        .attr('height', barHeight)
-        .attr('fill', function(d) {return d;});
+  const rects = d3.select('#svg').selectAll('rect')
+    .data(colorList);
+  rects.enter()
+    .append('rect')
+    .merge(rects)
+    .attr('x', function(d,i) {return widthPerBar * i; })
+    .attr('y', 0)
+    .attr('width',widthPerBar)
+    .attr('height', barHeight)
+    .attr('fill', function(d) {return d;});
 }
