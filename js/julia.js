7ad997af964c5c7ba1372eedb1221c4f88eec330
Blake Thomas <blake.thomas@id.me>
2020-04-04 20:37:48 -0400

    julia

--- js/julia.js
+++ js/julia.js
@@ -4,13 +4,15 @@
   const width = cWidth > 500 ? 500 : cWidth;
   const height = cHeight > 400 ? 400 : cHeight;
 
+  const cx = -0.8;
+  const cy = 0.156;
   let MAX = 64;
   let axis;
   const goal = {
-    x: -0.7900105002101,
-    y: -0.165003159937,
+    x: 0.2434913,
+    y: -0.25,
   };
-  const zoomFactor = 0.1;
+  const zoomFactor = 0.06;
   let scale = [];
   let lastTime;
 
@@ -34,11 +36,11 @@
 
   function setAxis() {
     const aspectRatio = height / width;
-    const startingWidth = 3.5;
+    const startingWidth = 4;
 
     axis = {
-      xmin: -2.5,
-      xmax: 1,
+      xmin: -2,
+      xmax: 2,
       ymin: (-aspectRatio * startingWidth) / 2,
       ymax: (aspectRatio * startingWidth) / 2,
     };
@@ -57,7 +59,7 @@
     let i = 0;
     while (i < MAX) {
       const rbg = d3
-        .interpolateTurbo(i / (MAX - 1))
+        .interpolateRainbow(i / (MAX - 1))
         .slice(4, -1)
         .split(',')
         .map((d) => +d);
@@ -71,14 +73,10 @@
     if (lastTime && currTime - lastTime < 50) {
       return window.requestAnimationFrame(draw);
     }
-    // const s = MAX;
-    // console.time(s);
     lastTime = currTime;
 
     let y = 0,
       x = 0,
-      xx = 0,
-      yy = 0,
       xn = 0,
       yn = 0,
       xi = 0,
@@ -86,20 +84,16 @@
 
     for (; y < height; y++) {
       for (x = 0; x < width; x++) {
-        xx = ((axis.xmax - axis.xmin) * x) / width + axis.xmin;
-        yy = ((axis.ymax - axis.ymin) * y) / height + axis.ymin;
-
-        xn = xx;
-        yn = yy;
-        xi = 0;
+        xn = ((axis.xmax - axis.xmin) * x) / width + axis.xmin;
+        yn = ((axis.ymax - axis.ymin) * y) / height + axis.ymin;
         t = -1;
 
         while (t++ < MAX) {
-          if (xn * xn + yn * yn > 3) {
+          if (xn * xn + yn * yn > 2) {
             break;
           }
-          xi = xn * xn - yn * yn + xx;
-          yn = 2 * xn * yn + yy;
+          xi = xn * xn - yn * yn + cx;
+          yn = 2 * xn * yn + cy;
           xn = xi;
         }
 
@@ -110,11 +104,10 @@
     ctx.putImageData(imageData, 0, 0);
 
     zoom();
-    if (MAX++ > 380) {
+    if (MAX++ > 320) {
       restart();
     }
 
-    // console.timeEnd(s);
     window.requestAnimationFrame(draw);
   }
 
