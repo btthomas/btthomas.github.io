eaac085f18848dde710c2c9b80697055807da98b
Blake Thomas <blake.thomas@id.me>
2020-04-18 14:26:19 -0400

    smoooooth

--- js/triangle.js
+++ js/triangle.js
@@ -1,11 +1,11 @@
 (function () {
   cWidth = document.body.clientWidth;
   cHeight = document.body.clientHeight;
-  const width = cWidth > 800 ? 800 : cWidth;
-  const height = cHeight > 600 ? 600 : cHeight;
+  const width = cWidth > 1200 ? 1200 : cWidth;
+  const height = cHeight > 1000 ? 1000 : cHeight;
 
-  const MAX_SPEED = 10;
-  const MAX = 11;
+  const MAX_SPEED = 16;
+  const MAX = 24;
   let iteration = 0;
   let z = 1;
   const zoomFactor = 2 ** 0.0125;
@@ -18,9 +18,9 @@
   const goalX = 2 * width;
 
   let lastTime;
-  const scale = d3.schemeSpectral[11];
-
-  document.querySelector('#restart').addEventListener('click', restart);
+  let currentColorScale;
+  let scale;
+  const select = document.getElementById('color');
 
   const canvas = document.querySelector('canvas');
   canvas.height = height;
@@ -32,8 +32,39 @@
   hiddenCanvas.width = width * 4;
   const hiddenCtx = hiddenCanvas.getContext('2d', { alpha: false });
 
-  setCanvas();
-  window.requestAnimationFrame(draw);
+  const hiddenCanvas2 = document.createElement('canvas');
+  hiddenCanvas2.height = height * 4;
+  hiddenCanvas2.width = width * 4;
+  const hiddenCtx2 = hiddenCanvas2.getContext('2d', { alpha: false });
+
+  init();
+
+  function init() {
+    setScale();
+    setCanvas();
+    window.requestAnimationFrame(draw);
+  }
+
+  window.handleChangeColor = function (str) {
+    setScale(str);
+  };
+
+  function setScale(d3Scale) {
+    if (d3Scale == undefined && currentColorScale == undefined) {
+      d3Scale = 'interpolateSinebow';
+      select.value = d3Scale;
+    }
+    if (d3Scale) {
+      currentColorScale = d3[d3Scale];
+    }
+    scale = [];
+    let i = 0;
+    while (i < MAX) {
+      const rbg = currentColorScale(i / (MAX - 1));
+      scale.push(rbg);
+      i++;
+    }
+  }
 
   function setCanvas() {
     hiddenCtx.fillStyle = scale[11];
@@ -101,6 +132,26 @@
     );
   }
 
+  function iterate2() {
+    // draw new color!
+    hiddenCtx2.fillStyle = scale[iteration++ % MAX];
+    hiddenCtx2.fillRect(0, 0, 4 * width, 4 * height);
+
+    // copy from hidden to hidden2 canvas 3 times.
+    hiddenCtx2.drawImage(hiddenCanvas, width, 0, 2 * width, 2 * height);
+    hiddenCtx2.drawImage(hiddenCanvas, 0, 2 * height, 2 * width, 2 * height);
+    hiddenCtx2.drawImage(
+      hiddenCanvas,
+      2 * width,
+      2 * height,
+      2 * width,
+      2 * height
+    );
+
+    // copy from hidden2 to hidden1
+    hiddenCtx.drawImage(hiddenCanvas2, 0, 0);
+  }
+
   function zoom() {
     axis = {
       xMin: zoomFactor * axis.xMin + (1 - zoomFactor) * goalX,
@@ -120,6 +171,7 @@
       width,
       height
     );
+    z = z * zoomFactor;
   }
 
   function draw(currTime) {
@@ -128,11 +180,9 @@
     }
     lastTime = currTime;
 
-    if (z <= 2) {
+    if (z < 2) {
       zoom();
-      z = z * zoomFactor;
     } else {
-      // reset axis
       axis = {
         xMin: width,
         xMax: 3 * width,
@@ -140,14 +190,9 @@
         yMax: 2 * height,
       };
       z = 1;
-
-      // iterate once
-      iterate();
+      iterate2();
+      zoom();
     }
     return window.requestAnimationFrame(draw);
-  }
-
-  function restart() {
-    const inputs = document.getElementById('inputs');
   }
 })();
